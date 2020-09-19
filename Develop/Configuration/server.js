a13ffb47feba1cac27a6e76229d4d19a9127ace6
Anissa Shanks <anissa.shanks@gmail.com>
2020-09-19 15:49:03 -0500

    added express app coding used to handle data parsing and mongo database connection code to server.js file

--- Develop/Configuration/server.js
+++ Develop/Configuration/server.js
@@ -9,21 +9,26 @@ const express = require ("express");
 const app = express ()
 const PORT = process.env.PORT || 8080
 
-app.use(morgan("dev"));
+const User = require (".UserModel.js");
+
+
+app.use(logger("dev"));
 
 // Express app used to handle data parsing
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
+app.use(express.static("public"));
 
 // Mongo database
-
-
-
+mongoose.connect(process.env.MONGOD_URI || "mongodb://localhost/custommethoddb", {useNewUrlParser}
 
 
 
 // Routes
 
+app.post
 
 
 // Spinning up server to listen for prompts
