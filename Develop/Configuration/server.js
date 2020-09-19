19f4bf605e79197fc0f6be7a63348a6de1d65bbc
Anissa Shanks <anissa.shanks@gmail.com>
2020-09-19 15:35:00 -0500

    set up server.js file and added code for dependencies, connecting express app

---
+++ Develop/Configuration/server.js
@@ -0,0 +1,35 @@
+// Dependencies 
+
+const mongoose =  require ("mongoose");
+const logger = require ("morgan");
+const express = require ("express");
+
+// Running and connecting express app
+
+const app = express ()
+const PORT = process.env.PORT || 8080
+
+app.use(morgan("dev"));
+
+// Express app used to handle data parsing
+
+
+
+// Mongo database
+
+
+
+
+
+
+// Routes
+
+
+
+// Spinning up server to listen for prompts
+console.log(`App listening on Port ${PORT}!`);
+});
+
+
+
+
